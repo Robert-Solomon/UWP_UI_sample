7a182b64fe9228e91ed7c21a80d51475f6ab9d9b
Robert Solomon <Robert-Solomon@users.noreply.github.com>
2019-06-20 16:44:50 -0700

    Adding MVC-Sample
    A sample application to learn development with ASP.NET MVC

---
+++ MVC-Sample/Controllers/HelloWorldController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MVC_Sample.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        // call as: HelloWorld/Welcome?name=Rick&numtimes=4
+
+        public string Welcome0(string name="NoName", int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+
+        //
+        // GET: /HelloWorld/Well
+        // call as: /helloworld/well/3?name=NAME
+        public string Welcome1(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+    }
+}
